4096b254d3e2d3e4907847fe688a062557ae3d52
MidnightChaos <Aidan.yung@gmail.com>
2025-01-30 15:28:45 -0700

    some more changes

--- Demo/Demo/demo.c
+++ Demo/Demo/demo.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main() {
+	int a[5];
 
+	a[0] = 2;
+	a[1] = 3;
+	a[2] = 7;
+
+	printf("sizeof(a) = %lu\n", sizeof(a));
+	printf("sizeof(a[0]) = %lu\n", sizeof(a[0]));
+
+	printf("a[2] = %d\n", a[2]);
+	printf("*a(a+2) = %d\n", *(a+2));
+	printf("a = %p\n", a);
+	return 0;
 }
