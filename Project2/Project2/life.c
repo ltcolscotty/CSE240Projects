27fdc2538a236a59a35555d3d2d7417aa8971a3a
MidnightChaos <aidan.yung@gmail.com>
2025-02-06 21:49:46 -0700

    initial testing state

--- Project2/Project2/life.c
+++ Project2/Project2/life.c
@@ -1,5 +1,4 @@
 /*
-* 
 * Aidan Yung
 * 2/6/2025
 * 
@@ -21,43 +20,72 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-#define NUM_ROWS = 5
-#define NUM_COLS = 5
-#define DEAD_CHAR = 'x'
-#define ALIVE_CHAR = 'o'
-#define GENERATIONS = 7
-#define TOKEN = ','
+#define NUM_ROWS 5
+#define NUM_COLS 5
+#define DEAD_CHAR 'x'
+#define ALIVE_CHAR 'o'
+#define GENERATIONS 7
+#define TOKEN_DELIM ","
+
 
 
 // use strtok
 // Will do its best to translate inputs into valid inputs
 // Out of range numbers will be discarded and characters will be casted to ints
-int GetInitialState(int *init_state[])
+int GetInitialState(int init_state[])
 {
-	
+	int validTokenCount = 0;
+	char inputStr[30];
+	printf("Input your start configuration: \n");
+	scanf(" %29s", &inputStr);
+
+	char* token;
+
+	// First token
+	token = strtok(inputStr, TOKEN_DELIM);
+
+	while (token != 0) {
+		int pos = atoi(token);
+		if ((pos < (NUM_ROWS * NUM_COLS)) && 
+			(pos >= 0) && 
+			(validTokenCount < (NUM_ROWS * NUM_COLS)))
+		{
+			init_state[validTokenCount] = pos;
+			validTokenCount++;
+		}
+
+		token = strtok(0, TOKEN_DELIM);
+	}
+
+	//clear buffer
+	while (getchar() != '\n');
+	return validTokenCount;
 }
 
-void SetInitialState(char *board[][NUM_COLS], int init_state[], int num_alive)
+void SetInitialState(int board[][NUM_COLS], int init_state[], int num_alive)
 {
 
 }
 
-void PrintBoard(char board[][NUM_COLS])
+void PrintBoard(int board[][NUM_COLS])
 {
 
 }
 
 // Produce a second 2d array then replace first array memory with it
-void NextGeneration(char *board1[][NUM_COLS])
+void NextGeneration(int board1[][NUM_COLS])
 {
 
 }
 
-int CountLiveNeighbors(char board[][NUM_COLS], int r, int c)
+int CountLiveNeighbors(int board[][NUM_COLS], int r, int c)
 {
 	int aliveNeighborCt = 0;
 
+
+	return aliveNeighborCt;
 }
 
 void PlayGameOfLife()
@@ -68,7 +96,14 @@ void PlayGameOfLife()
 int main()
 {
 	int board[NUM_ROWS][NUM_COLS];
+	int initialState[NUM_ROWS * NUM_COLS];
+	int count = GetInitialState(initialState);
 
+	// testing call
+	for (int i = 0; i < (count); i++)
+	{
+		printf("%d\n",initialState[i]);
+	}
 
 	return 0;
 }
