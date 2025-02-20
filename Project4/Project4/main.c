eb895f8299765c460e8ff101361467e53a0b23c0
MidnightChaos <Aidan.yung@gmail.com>
2025-02-20 16:30:33 -0700

    defined basic functions

--- Project4/Project4/main.c
+++ Project4/Project4/main.c
@@ -2,6 +2,27 @@
 #include <stdlib.h>
 #include "slist.h"
 
+typedef SList Deque;
+
+void push_back(Deque* q, char* data)
+{
+
+}
+
+void push_front(Deque* q, char* data)
+{
+
+}
+
+char* pop_back(Deque* q)
+{
+
+}
+
+char* pop_front(Deque* q)
+{
+
+}
 int main()
 {
 	return 0;
---
+++ Project4/Project4/slist.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "slist.h"
+
+void insertHead(SList* list, char* data)
+{
+
+}
+
+char* removeHead(SList* list)
+{
+
+}
+
+void insertTail(SList* list, char* data)
+{
+
+}
+
+char* removeTail(SList* list)
+{
+
+}
