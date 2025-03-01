791d0ef3eaa390bf90d98b7eae24c81c51411b08
MidnightChaos <aidan.yung@gmail.com>
2025-02-28 19:42:20 -0700

    call me a data entry specialist

--- Project5/Project5/twenty.c
+++ Project5/Project5/twenty.c
@@ -2,6 +2,8 @@
 * Aidan Yung
 * 2/27/2025
 * Twenty Questions Project
+* 
+* Assuming 20 questions means the total number of questions in the tree
 */
 
 #include <stdio.h>
@@ -15,6 +17,62 @@ node* buildTree()
 	node* root = NULL;
 
 	root = InsertNode(root, 500, "Does it grow underground", "");
+	
+	// Left side (Yes)
+
+	InsertNode(root, 250, "q", "");
+	InsertNode(root, 375, "q", "");
+	InsertNode(root, 350, "q", "");
+	InsertNode(root, 425, "q", "");
+	InsertNode(root, 400, "q", "");
+	InsertNode(root, 450, "q", "");
+	InsertNode(root, 125, "q", "");
+	InsertNode(root, 175, "q", "");
+	InsertNode(root, 200, "q", "");
+	InsertNode(root, 60, "q", "");
+	InsertNode(root, 30, "q", "");
+	InsertNode(root, 100, "q", "");
+
+	// Left side answers
+
+	InsertNode(root, 30, "", "a");
+	InsertNode(root, 100, "", "a");
+	InsertNode(root, 150, "", "a");
+	InsertNode(root, 180, "", "a");
+	InsertNode(root, 225, "", "a");
+	InsertNode(root, 325, "", "a");
+	InsertNode(root, 360, "", "a");
+	InsertNode(root, 390, "", "a");
+	InsertNode(root, 415, "", "a");
+	InsertNode(root, 435, "", "a");
+	InsertNode(root, 475, "", "a");
+
+	// Right side (No)
+
+	InsertNode(root, 750, "q", "");
+	InsertNode(root, 675, "q", "");
+	InsertNode(root, 600, "q", "");
+	InsertNode(root, 550, "q", "");
+	InsertNode(root, 640, "q", "");
+	InsertNode(root, 725, "q", "");
+	InsertNode(root, 875, "q", "");
+	InsertNode(root, 825, "q", "");
+	InsertNode(root, 800, "q", "");
+	InsertNode(root, 925, "q", "");
+
+	// Right side answers
+	InsertNode(root, 525, "", "a");
+	InsertNode(root, 575, "", "a");
+	InsertNode(root, 630, "", "a");
+	InsertNode(root, 650, "", "a");
+	InsertNode(root, 700, "", "a");
+	InsertNode(root, 735, "", "a");
+	InsertNode(root, 775, "", "a");
+	InsertNode(root, 815, "", "a");
+	InsertNode(root, 850, "", "a");
+	InsertNode(root, 900, "", "a");
+	InsertNode(root, 950, "", "a");
+
 
 	return root;
 }
