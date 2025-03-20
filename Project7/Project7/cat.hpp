92d26af4cf51f447cc91e84ce34d038bb5172aa8
MidnightChaos <aidan.yung@gmail.com>
2025-03-19 23:31:03 -0700

    i hate vs2022 like that

--- Project7/Project7/cat.hpp
+++ Project7/Project7/cat.hpp
@@ -10,7 +10,7 @@ public:
     Cat() : Pet("", 0) {}
     Cat(std::string name, int weight) : Pet(name, weight) {}
 protected:
-    float GetLicensingRate() const override { return 1.5f; }
+    float GetLicensingRate() const override { return 1.5f; } // Using this instead of a variable because VS2022 seems to want me to jump through some hoops to use a static var and we aren't submitting a cat.cpp file
 };
 
 #endif // !cat_hpp
