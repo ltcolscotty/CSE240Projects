85b338860bddd69315e0465a90e070f85c95d883
MidnightChaos <aidan.yung@gmail.com>
2025-03-27 17:45:47 -0700

    infinite regret

--- Project8/Project8/BinarySearchTree.hpp
+++ Project8/Project8/BinarySearchTree.hpp
@@ -6,7 +6,7 @@ class Node
 {
 public:
 	Node() {}
-	Node(const K& key, V& value) : key(key), data(value), left(NULL), right(NULL), height(1) {}
+	Node(const K& key, V& value) : key(key), data(value), left(nullptr), right(nullptr), height(1) {}
 
 	int height;
 	K key;
@@ -14,6 +14,60 @@ public:
 	Node* left;
 	Node* right;
 };
+
+template <typename K, typename V>
+int height(Node<K, V>* n)
+{
+	if (n == nullptr)
+		return 0;
+	return n->height;
+}
+
+template <typename K, typename V>
+Node<K, V>* rightRotate(Node<K, V>* n)
+{
+	if (n == nullptr || n->left == nullptr)
+	{
+		return n;
+	}
+
+	// Temporary references
+	Node<K, V>* n1 = n->left;
+	Node<K, V>* n2 = n1->right;
+
+	// Rotate
+	n1->right = n;
+	n->left = n2;
+
+	//Height update
+	n->height = 1 + std::max(height(n->left), height(n->right));
+	n1->height = 1 + std::max(height(n1->left), height(n1->right));
+
+	return n1;
+}
+
+template <typename K, typename V>
+Node<K, V>* leftRotate(Node<K, V>* n)
+{
+	if (n == nullptr || n->right == nullptr)
+	{
+		return n;
+	}
+
+	// Temporary references
+	Node<K, V>* n1 = n->right;
+	Node<K, V>* n2 = n->left;
+
+	// Rotate
+	n1->left = n;
+	n->right = n2;
+
+	//Height update
+	n->height = 1 + std::max(height(n->left), height(n->right));
+	n1->height = 1 + std::max(height(n1->left), height(n1->right));
+
+	return n1;
+}
 
 template <typename K, typename V>
 class BinarySearchTree
@@ -26,7 +80,7 @@ public:
 	bool contains(K key);
 	V find(K key);
 private:
-
+	Node* head;
 };
 
 template <typename K, typename V>
