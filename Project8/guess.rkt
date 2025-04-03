5e3be097f8a761da9685e4f0ecdd6b18abb9a14e
MidnightChaos <Aidan.yung@gmail.com>
2025-04-03 12:38:20 -0700

    initial commit

---
+++ Project8/guess.rkt
@@ -0,0 +1,34 @@
+(#%require (lib "27.ss" "srfi"))
+(random-source-randomize! default-random-source)
+
+
+(define get_guess
+  (lambda (target guesses)
+    (display "Enter guess: " )
+     (let ((guess (read)))
+    (cond
+      ((= guess target)
+       (display "Correct! You made ")
+       (display guesses)
+       (display " guesses..." )
+       (newline)
+       (display "Please enter your name: ")
+       (let ((name (read)))
+         (display "Good game, ")
+         (display name)
+         (display "!")))
+      ((< guess target)
+       (display "Higher...")
+       (get_guess target (+ guesses 1)))
+      ((> guess target)
+       (display "Lower...")
+       (get_guess target (+ guesses 1)))
+       ))))
+
+(define play_guess
+  (lambda (target init)
+    (display "Guess a number from 1 to 100")
+    (newline)
+    (get_guess target init)))
+
+(play_guess (+ (random-integer 99) 1) 1)
