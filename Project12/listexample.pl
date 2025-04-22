f6c92616516972bbdf9633a7f172009ebf099592
MidnightChaos <Aidan.yung@gmail.com>
2025-04-22 14:19:38 -0700

    more thing

--- Project12/listexample.pl
+++ Project12/listexample.pl
@@ -3,3 +3,22 @@ mother_of(ariyah, [mike, sarah, george]).
 mother_of(elaine, [tom, dick]).
 
 mother(X, Y) :-  mother_of(X, Z), member(Y, Z).
+
+change(H, Q, D, N, P) :-
+    member(H, [0,1,2]),
+    member(Q, [0,1,2,3,4]),
+    member(D, [0,1,2,3,4,5,6,7,8,9,10]),
+    member(N, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]),
+    S is 50*H + 25*Q + 10*D + 5*N,
+    S =< 100,
+    P is 100-S.
+
+first(X, [X|_]).
+car(X, [X|_]).
+second(X,[_,X|_]).
+cadr(X,[_,X|_]).
+
+last(X,[X]).
+last(X,[_|T]) :- last(X, T).
+
+addhead(List, Element, [Element | List]).
