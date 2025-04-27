88db63b80b3ee0587a1b42092270e939d82f32fc
MidnightChaos <Aidan.yung@gmail.com>
2025-04-27 01:27:50 -0700

    im so tired

--- Project12/msort.pl
+++ Project12/msort.pl
@@ -1,5 +1,24 @@
-split(L, P1, P2)
+split(L, P1, P2) :-
 
+/*guh think about this later*/
+merge([A1|R1], [A2|R2], Merged) :-
+    A1 >= A2,
+    [A]
+
+/*base cases*/
+mergeSort([], []).
+mergeSort([A], [A]).
+
+/*Thinking stuff
+L1 - input
+L2 - output
+
+P1 - part 1
+P2 - part 2
+
+S1 - sorted 1
+S2 - sorted 2
+*/
 mergeSort(L1, L2) :-
     split(L1, P1, P2),
     mergeSort(P1, S1),
